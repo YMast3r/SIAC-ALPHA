816dac548dc97e4132390b98bee6eadce02b56ad
iva132 <22301061550320@cetis155.edu.mx>
2024-10-15 18:14:47 -0600

    Cambios en back-end. 1.-  Se cambió los elementos en la base de datos. 2.-  Se emplearon los dominios del correo.

--- src/funciones/correo.js
+++ src/funciones/correo.js
@@ -1,11 +1,11 @@
 const correoInput = document.getElementById('correo_electronico');
 const correoError = document.getElementById('correoError');
-const correoPattern = /^[A-Za-z0-9._%+-]+@(gmail\.com|yahoo\.com|outlook\.com)$/; // Patrón de correos válidos
+const correoPattern = /^[A-Za-z0-9._%+-]+@(gmail\.com|yahoo\.com|outlook\.com|cetis155\.edu\.mx|hotmail\.com|aol\.com|icloud\.com)$/; // Patrón de correos válidos
 
 correoInput.addEventListener('input', function () {
     const correoValue = correoInput.value.trim();
-    if (!correoPattern.test(correoValue) || correoValue.length < 16 || correoValue.length > 30) {
-        correoError.textContent = 'Ingrese un correo válido (ej. usuario@gmail.com) de 6 a 30 caracteres';
+    if (!correoPattern.test(correoValue) || correoValue.length < 10 || correoValue.length > 50) { // Ajustamos las longitudes
+        correoError.textContent = 'Ingrese un correo válido (ej. usuario@gmail.com) de 10 a 50 caracteres';
     } else {
         correoError.textContent = '';
     }
@@ -15,9 +15,8 @@ function correoValid() {
     const correoInput = document.getElementById('correo_electronico');
     const correoError = document.getElementById('correoError');
     const correoValue = correoInput.value.trim();
-
-    if (!correoPattern.test(correoValue) || correoValue.length < 16 || correoValue.length > 30) {
-        correoError.textContent = 'Error al mandar: Ingrese un correo válido (ej. usuario@gmail.com) de 6 a 30 caracteres';
+    if (!correoPattern.test(correoValue) || correoValue.length < 10 || correoValue.length > 50) { // Ajustamos las longitudes
+        correoError.textContent = 'Error al mandar: Ingrese un correo válido (ej. usuario@gmail.com) de 10 a 50 caracteres';
         return false;
     } else {
         correoError.textContent = '';
