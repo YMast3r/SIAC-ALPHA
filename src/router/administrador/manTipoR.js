d537a7006f198aec025c086bee00fcce9a82d47e
iva132 <22301061550320@cetis155.edu.mx>
2024-10-17 00:20:44 -0600

    Desarrollo de back-end. - Se agregó tipo usuario en la página tipos, se agregó un diccionario para los datos. - Se arregló el problema que no mostraba el mensaje cuando no se encontraba una descripción. - Se cambió el contenido del Popup, para que se entienda mejor.

--- src/router/administrador/manTipoR.js
+++ src/router/administrador/manTipoR.js
@@ -3,12 +3,13 @@ const manTipo = require('../../controllers/administrador/manTipo');
 const router = express.Router();
 
 router.get('/renderManTipo', manTipo.renderManTipo);
-// Ruta para renderizar manTipo general (ejemplo con 'tipo_pago')
+// Ruta para renderizar manTipoPago general (ejemplo con 'tipo_pago')
 router.get('/manTipoPago', (req, res) => {
     const formFields = [
         { label: 'Descripción', type: 'text', name: 'descripcion', required: true },
         { label: 'Precio', type: 'tel', name: 'precio', required: true }
     ];
+    req.session.errorBorrarR = 'tipo_pago';
     manTipo.manTipo(req, res, 'tipo_pago', 'id_tipo_pago', 'descripcion', formFields);
 });
 
@@ -17,7 +18,17 @@ router.get('/manTipoIncidencia', (req, res) => {
     const formFields = [
         { label: 'Descripción', type: 'text', name: 'descripcion', required: true }
     ];
+    req.session.errorBorrarR = 'tipo_incidencia';
     manTipo.manTipo(req, res, 'tipo_incidencia', 'id_tipo_incidencia', 'descripcion', formFields);
+});
+
+// Ruta para renderizar manTipoUsuario general (ejemplo con 'altaTipo')
+router.get('/manTipoUsuario', (req, res) => {
+    const formFields = [
+        { label: 'Descripción', type: 'text', name: 'descripcion', required: true }
+    ];
+    req.session.errorBorrarR = 'tipo_usuario';
+    manTipo.manTipo(req, res, 'tipo_usuario', 'id_tipo_usuario', 'descripcion', formFields);
 });
 
 // Ruta para manejar el alta de un tipo de pago
