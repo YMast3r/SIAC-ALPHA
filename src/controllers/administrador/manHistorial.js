41af83e697192f0375fb33c4f3bf20246f1c5b96
Angel <22301061550335@cetis155.edu.mx>
2024-10-20 16:02:36 -0600

    Estructura de historial f Se creo la estructura de historial, se agrego las pestañas para los historiales de incidencias, pagos, condominos y propiedades. cada una con sus tablas y sus funciones conectadas a las rutas

--- src/controllers/administrador/manHistorial.js
+++ src/controllers/administrador/manHistorial.js
@@ -1,3 +1,5 @@
+const { manCondomino } = require("./condomino/manCondomino");
+
 function renHistorial(req, res) {
     res.render('usuarios/administrador/manHistorial', {
         name: req.session.name,
@@ -5,6 +7,227 @@ function renHistorial(req, res) {
     });
 }
 
+function manPagos(req, res) {
+    // Recuperamos el id de la propiedad (puede venir de la sesión o la ruta)
+    const idPropiedad = req.session.idPropiedad || req.params.idPropiedad;
+
+    req.getConnection((err, conn) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        conn.query(`SELECT a.folio, a.id_propiedad AS propiedad, FORMAT(a.importe, 2) AS importe, t.descripcion AS tipo_pago, a.fecha, b.descripcion AS mes, a.año, COALESCE(a.numero_recibo, 'Indefinido') AS numero_recibo,  COALESCE(a.referencia, 'Indefinido') AS referencia FROM pago a JOIN mes b ON a.mes = b.mes JOIN tipo_pago t ON a.tipo_pago = t.id_tipo_pago WHERE a.id_propiedad = ?ORDER BY a.folio DESC`, [idPropiedad], (err, rows) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+
+            if (rows.length > 0) {
+                // Mapeamos los datos para formatear la fecha
+                const pagos = rows.map(row => ({
+                    ...row,
+                    fecha: formatDate(row.fecha), // Asegúrate de tener la función formatDate
+                }));
+
+                // Renderizamos la vista con los pagos
+                return res.render('usuarios/administrador/pagos', {
+                    pagos: pagos,
+                    name: req.session.name,
+                    id: req.session.idUser,
+                    tipoUsuario: req.session.tipoUsuario
+                });
+            } else {
+                // Si no hay pagos, mostramos un mensaje de error
+                return res.render('usuarios/administrador/pagos', {
+                    errorDatos: 1,
+                    name: req.session.name,
+                    id: req.session.idUser,
+                    tipoUsuario: req.session.tipoUsuario
+                });
+            }
+        });
+    });
+}
+
+function manIncidencias(req, res) {
+    // Obtén el ID del usuario desde la sesión o la ruta
+    const idUsuario = req.session.idUser || req.params.id;
+
+    // Determina el tipo y la ruta dependiendo del tipo de usuario
+    let tipo;
+    let ruta;
+    if (req.session.tipoUsuario == 3) {
+        tipo = 3; // Condómino
+        ruta = 'usuarios/condomino/manIncidencias';
+    } else {
+        tipo = 2; // Administrador
+        ruta = 'usuarios/administrador/manIncidencias';
+    }
+
+    req.getConnection((err, conn) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+
+        // Consulta para recuperar las incidencias
+        conn.query(`SELECT a.folio, d.nombre AS usuario, b.descripcion AS tipo_incidencia, a.fecha, a.descripcion FROM incidencia a JOIN tipo_incidencia b ON a.id_tipo_incidencia = b.id_tipo_incidencia JOIN usuario d ON a.id_usuario = d.id_usuario WHERE a.id_usuario = ? ORDER BY a.folio DESC`, [idUsuario], (err, rows) => {
+                if (err) {
+                    console.log(err);
+                    return;
+                }
+
+                if (rows && rows.length > 0) {
+                    const incidencias = rows.map(row => ({
+                        ...row,
+                        fecha: formatDate(row.fecha), // Asegúrate de tener la función formatDate
+                    }));
+
+                    // Renderiza la vista con las incidencias
+                    return res.render(ruta, {
+                        incidencias: incidencias,
+                        name: req.session.name,
+                        id: req.session.idUser,
+                        tipoUsuario: tipo
+                    });
+                } else {
+                    console.log('No se encontraron incidencias');
+                    return res.render(ruta, {
+                        name: req.session.name,
+                        id: req.session.idUser,
+                        tipoUsuario: tipo,
+                        errorDatos: 1, // O un mensaje que desees mostrar
+                    });
+                }
+            });
+    });
+}
+
+function manCondominos(req, res) {
+    const usuario = req.session.dataCampos;
+    const error = req.session.errorMC;
+
+    req.getConnection((err, conn) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        
+        // Consulta para obtener los condóminos
+        conn.query(`SELECT a.id_usuario AS id, a.nombre, a.correo_electronico, a.telefono, GROUP_CONCAT(b.descripcion SEPARATOR ', ') AS propiedades FROM usuario a LEFT JOIN propiedad b ON a.id_usuario = b.id_usuario WHERE a.tipo_usuario = 3 GROUP BY a.id_usuario ORDER BY a.id_usuario DESC`, 
+        (err, rows) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            
+            if (rows.length > 0) {
+                const datos = rows;
+                res.render('usuarios/administrador/condomino/manCondominos', { 
+                    datos: datos, 
+                    name: req.session.name, 
+                    tipoUsuario: 2, 
+                    usuario: usuario, 
+                    error: error 
+                });
+                return;
+            } else {
+                res.render('usuarios/administrador/condomino/manCondominos', { 
+                    name: req.session.name, 
+                    tipoUsuario: 2, 
+                    usuario: usuario, 
+                    error: error 
+                });
+                return;
+            }
+        });
+    });
+}
+
+function manPropiedad(req, res) {
+    const error = req.session.errorMPro;
+    const errorT = req.session.errorMT;
+    const data = req.session.dataCampos;
+    const altaT = req.session.altaT;
+
+    req.getConnection((err, conn) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+
+        // Consulta para obtener los condóminos
+        conn.query('SELECT nombre, id_usuario FROM usuario WHERE tipo_usuario = 3', (err, rows) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            if (rows.length > 0) {
+                const condomino = rows;
+
+                // Consulta para obtener los tipos de propiedad
+                conn.query('SELECT id_tipo_propiedad, descripcion FROM tipo_propiedad ORDER BY id_tipo_propiedad DESC', (err, rows) => {
+                    if (err) {
+                        console.log(err);
+                        return;
+                    }
+                    if (rows.length > 0) {
+                        const tipo = rows;
+
+                        // Consulta para obtener las propiedades
+                        conn.query(`SELECT a.id_propiedad, a.descripcion, COALESCE(u.nombre, 'Indefinido') AS condomino, b.descripcion AS tipo_propiedad FROM propiedad a LEFT JOIN usuario u ON a.id_usuario = u.id_usuario JOIN tipo_propiedad b ON a.id_tipo_propiedad = b.id_tipo_propiedad ORDER BY a.id_propiedad DESC`, 
+                        (err, rows) => {
+                            if (err) {
+                                console.log(err);
+                                return;
+                            }
+                            if (rows.length > 0) {
+                                const propiedad = rows;
+                                return res.render('usuarios/administrador/manPropiedad', {
+                                    name: req.session.name,
+                                    tipoUsuario: 2,
+                                    tipoPropiedad: tipo,
+                                    propiedad: propiedad,
+                                    condomino: condomino,
+                                    data: data,
+                                    altaT: altaT,
+                                    error: error,
+                                    errorT: errorT
+                                });
+                            } else {
+                                return res.render('usuarios/administrador/manPropiedad', {
+                                    name: req.session.name,
+                                    tipoUsuario: 2,
+                                    tipoPropiedad: tipo,
+                                    propiedad: [],
+                                    condomino: condomino,
+                                    data: data,
+                                    altaT: altaT,
+                                    error: error,
+                                    errorT: errorT
+                                });
+                            }
+                        });
+                    } else {
+                        return res.render('usuarios/administrador/manPropiedad', {
+                            name: req.session.name,
+                            tipoUsuario: 2,
+                            data: data,
+                            altaT: altaT,
+                            condomino: condomino,
+                            error: error,
+                            errorT: errorT
+                        });
+                    }
+                });
+            }
+        });
+    });
+}
 module.exports = { 
-    renHistorial 
+    renHistorial,
+    manPagos,
+    manIncidencias,
+    manCondominos,
+    manPropiedad
 };
--- src/router/administrador/manHistorialR.js
+++ src/router/administrador/manHistorialR.js
@@ -3,5 +3,9 @@ const manHistorial = require('../../controllers/administrador/manHistorial.js');
 const router = express.Router();
 
 router.get('/renHistorial', manHistorial.renHistorial);
+router.get('/manPagos', manHistorial.manPagos);
+router.get('/manIncidencias', manHistorial.manIncidencias);
+router.get('/manCondominos', manHistorial.manCondominos);
+router.get('/manPropiedad', manHistorial.manPropiedad);
 
 module.exports = router;
